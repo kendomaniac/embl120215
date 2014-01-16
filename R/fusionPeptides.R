f3949b2fddc07450b296b100c9d460e11b8d4be1
Raffaele Calogero <raffaele.calogero@unito.it>
2014-01-16 15:47:37 +0000

    fixing bugs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chimera@85569 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fusionPeptides.R
+++ R/fusionPeptides.R
@@ -4,6 +4,7 @@ fusionPeptides <- function(chimeraSeq.output, annotation="hsUCSC"){
 		return()
     }
 	ann <- as.list(org.Hs.egUCSCKG)
+	symbol <- as.list(org.Hs.egSYMBOL)
 	f.name <- names(chimeraSeq.output)
 	f.name <- strsplit(f.name, ":|\\.")
 	f.name <- c(f.name[[1]][1],f.name[[1]][3])
@@ -14,6 +15,9 @@ fusionPeptides <- function(chimeraSeq.output, annotation="hsUCSC"){
 		 else
 			 return(NA)
 	 },y=f.name[1])
+	id1.sym <- as.character(symbol[which(names(symbol)==names(id1[!is.na(id1)]))])
+	if(length(id1.sym)==0)
+		id1.sym <- NA 
 	id1 <- as.character(id1[!is.na(id1)])
 	id2 <- lapply(ann, function(x,y){
 		 tmp <- grep(y,x)
@@ -22,15 +26,19 @@ fusionPeptides <- function(chimeraSeq.output, annotation="hsUCSC"){
 		 else
 			 return(NA)
 	 },y=f.name[2])
+	id2.sym <- as.character(symbol[which(names(symbol)==names(id2[!is.na(id2)]))])
+	if(length(id2.sym)==0)
+		id2.sym <- NA 
 	id2 <- as.character(id2[!is.na(id2)])
 	txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene 
 	cds_by_tx <- cdsBy(txdb, by="tx", use.names=TRUE)
-	trs <- c(id1,id2) 
+	trs <- c(id1,id2)
+	trs.sym <- c(id1.sym, id2.sym) 
 	if(length(which(trs %in% names(cds_by_tx)))==1){
-		cat("\nOnly names(cds_by_tx)[which(trs %in% names(cds_by_tx))] is coding\n")
+		cat("\nOnly ",paste(trs.sym[which(trs %in% names(cds_by_tx))],trs[which(trs %in% names(cds_by_tx))],sep="/")," is coding\n")
 		return()
 	}else if(length(which(trs %in% names(cds_by_tx)))==0){
-		cat("\nNon of the two genes is coding\n")
+		cat("\nNone of the two genes is coding\n")
 		return()
 	}
 	cds_seqs <- extractTranscriptsFromGenome(BSgenome.Hsapiens.UCSC.hg19, cds_by_tx[which(names(cds_by_tx) %in% trs)]) 
@@ -52,21 +60,45 @@ fusionPeptides <- function(chimeraSeq.output, annotation="hsUCSC"){
 	alignment.p2 <- alignment.p2[which(scores.p2==max(scores.p2))]
     if(length(which(scores.p1==max(scores.p1)))==1 & length(which(scores.p2==max(scores.p2)))==1){
 	    if(which(scores.p1==max(scores.p1)) == which(scores.p2==max(scores.p2))){
-		   cat("\nfused proteins are in frame\n")
 		   pattern1 <- as.character(pattern(alignment.p1[[1]]))
 		   fp1 <- matchPattern(pattern=pattern1, subject=fusion.p[[which(scores.p1==max(scores.p1))]])
 		   pattern2 <- as.character(pattern(alignment.p2[[1]]))
 		   fp2 <- matchPattern(pattern=pattern2, subject=fusion.p[[which(scores.p2==max(scores.p2))]])
-		   fusionp <- AAStringSet(c(paste(as.character(fp1), as.character(fp2), sep=""),as.character(fp1),as.character(fp2),as.character(p1),as.character(p2)))
-	       names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")
+		   if(length(as.character(fp1))==1 & length(as.character(fp2))==1){
+		       cat("\nfused proteins are in frame\n")
+    		   fusionp <- AAStringSet(c(paste(as.character(fp1), as.character(fp2), sep=""),as.character(fp1),as.character(fp2),as.character(p1),as.character(p2)))
+	           names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")
+		   }else if(length(as.character(fp1))==0 & length(as.character(fp2))==1){
+		   	  cat("\n",id1.sym," does not provide any good match pattern with fusion protein frame: ",which(scores.p1==max(scores.p1)),"\n")
+   		      fusionp <- AAStringSet(c("","",as.character(fp2),as.character(p1),as.character(p2)))
+              names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")			  
+			  #if this happen it is possible some issue in the fusion transcript reconstruction
+		   }else if(length(as.character(fp1))==1 & length(as.character(fp2))==0){
+		   	  cat("\n",id2.sym," does not provide any good match pattern with fusion protein frame: ",which(scores.p2==max(scores.p2)),"\n")
+			  #if this happen it is possible some issue in the fusion transcript reconstruction
+   		      fusionp <- AAStringSet(c("",as.character(fp1),"",as.character(p1),as.character(p2)))
+              names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")
+		   }
 		}else{
-	 	   cat("\nfused proteins are not in frame\n")
  		   pattern1 <- as.character(pattern(alignment.p1[[1]]))
  		   fp1 <- matchPattern(pattern=pattern1, subject=fusion.p[[which(scores.p1==max(scores.p1))]])
  		   pattern2 <- as.character(pattern(alignment.p2[[1]]))
  		   fp2 <- matchPattern(pattern=pattern2, subject=fusion.p[[which(scores.p2==max(scores.p2))]])
-		   fusionp <- AAStringSet(c("",as.character(fp1),as.character(fp2),as.character(p1),as.character(p2)))
-		   names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")		
+		   if(length(as.character(fp1))==1 & length(as.character(fp2))==1){
+			  cat("\nfused proteins are not in frame\n")
+		      fusionp <- AAStringSet(c("",as.character(fp1),as.character(fp2),as.character(p1),as.character(p2)))
+		      names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")
+		   }else if(length(as.character(fp1))==0 & length(as.character(fp2))==1){
+		   	  cat("\n",id1.sym," does not provide any good match pattern with fusion protein frame: ",which(scores.p1==max(scores.p1)),"\n")
+   		      fusionp <- AAStringSet(c("","",as.character(fp2),as.character(p1),as.character(p2)))
+              names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")			  
+			  #if this happen it is possible some issue in the fusion transcript reconstruction
+		   }else if(length(as.character(fp1))==1 & length(as.character(fp2))==0){
+		   	  cat("\n",id2.sym," does not provide any good match pattern with fusion protein frame: ",which(scores.p2==max(scores.p2)),"\n")
+   		      fusionp <- AAStringSet(c("",as.character(fp1),"",as.character(p1),as.character(p2)))
+              names(fusionp) <- c("fusion","p1pep","p2pep","p1","p2")
+			  #if this happen it is possible some issue in the fusion transcript reconstruction
+		   }		
 		}
 	}else if(length(which(scores.p1==max(scores.p1)))==1){
  	 	   cat("\nfused proteins are not in frame\np2 is not providing a unique good alignment to the fusion\nIt is possible that fusion involves non-coding UTR.\n")
